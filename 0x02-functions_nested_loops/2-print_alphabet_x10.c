308fdccb9b56a728953a0fed6b6787747cf24eb2
Donia-Farouk <fdonia726@gmail.com>
2023-08-15 15:36:10 -0700

    new

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-print_alphabet_x10 - prints alphabet letters 10 times followed by a new line.
+*print_alphabet_x10 - prints alphabet letters 10 times followed by a new line.
 *Return: void
 */
 void print_alphabet_x10(void)
